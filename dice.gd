93300c8ab833ae15d176f6792e25537446163da7
eymeric <eymericdechelette@gmail.com>
2024-07-16 20:15:05 +0200

    clickable dice

--- dice.gd
+++ dice.gd
@@ -15,14 +15,14 @@ func roll():
 	start_animation()
 	await get_tree().create_timer(1.0).timeout
 	stop_animation()
-	roll_value = $AnimatedSprite2D.frame + 1
+	roll_value = $Button/AnimatedSprite2D.frame + 1
 	return roll_value
 
 func start_animation():
-	$AnimatedSprite2D/AnimationPlayer.play("dice_animation")
+	$Button/AnimatedSprite2D/AnimationPlayer.play("dice_animation")
 	
 func stop_animation():
-	$AnimatedSprite2D/AnimationPlayer.stop()
+	$Button/AnimatedSprite2D/AnimationPlayer.stop()
 
 func toggle_animation():
 	if is_rolling():
@@ -30,11 +30,16 @@ func toggle_animation():
 	else:
 		start_animation()
 
-func is_rolling() -> bool: 
-	return $AnimatedSprite2D/AnimationPlayer.is_playing()
+func is_rolling() -> bool:
+	return $Button/AnimatedSprite2D/AnimationPlayer.is_playing()
 	
 func random_frame():
 	# Don't want to linger on one side
-	var last_frame = $AnimatedSprite2D.frame
-	while $AnimatedSprite2D.frame == last_frame:
-		$AnimatedSprite2D.frame = randi() % randi() % 6
+	print("random")
+	var last_frame = $Button/AnimatedSprite2D.frame
+	while $Button/AnimatedSprite2D.frame == last_frame:
+		$Button/AnimatedSprite2D.frame = randi() % randi() % 6
+
+func _on_button_pressed():
+	if (not is_rolling()):
+		await roll()
--- main.gd
+++ main.gd
@@ -7,12 +7,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _on_button_pressed():
-	var value: int;
-	if (not $Dice.is_rolling()):
-		value = await $Dice.roll()
-	else:
-		value = $Dice.roll_value
-	print(value)
