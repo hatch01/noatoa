826524f1aa8aa2eb01f1d7dd72e6ba192ab25df4
eymeric <eymericdechelette@gmail.com>
2024-07-31 22:41:33 +0200

    most of the collision are okay

--- main.gd
+++ main.gd
@@ -12,32 +12,77 @@ func _ready():
 	randomize()
 	for i in range(player_count):
 		var player = player_scene.instantiate()
-		var spawnLocation = Vector2(i*(55) + 225,900-(45/2)-3)
+		var spawnLocation = Vector2(i*(50) + 168,88)
 		player.position = spawnLocation
 		add_child(player)
 		players.append(player)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var bark_count: int = 0
+	pass
+"""	var bark_count: int = 0
 	for i in players:
 		if i.on_bark:
 			bark_count += 1
 		elif not i.on_bridge:
 			print("GAMEOVER")
 	if (bark_count > 1):
-		print("LA BARQUE ES PLEINE")
+		print("LA BARQUE ES PLEINE")"""
 
 func next_player():
+	var pos = players[current_player].pos()
+	if ((pos.x > 1 or pos.x<0) or (pos.y > 14)) and pos != Vector2(296,424):
+		print("plouf !")
 	if current_player + 1 >= player_count:
 		current_player = 0
 		round_count += 1
 	else:
 		current_player += 1
+	if players[current_player].on_bark:
+		players[current_player].iterate_boat()
 
 func _on_button_pressed():
+	if players[current_player].on_bark:
+		print("passe un tour")
+		next_player()
+		return
 	await $Dice.roll()
-	players[current_player].forward($Dice.roll_value)
-	next_player()
+	match $Dice.roll_value:
+		1, 2, 3:
+			go(Vector2(0, $Dice.roll_value))
+			next_player()
+		4:
+			print("vous devez relancer le dé")
+		5:
+			if players[current_player].position.x < get_window().size.x/2:
+				go(Vector2(1, 0))
+			else:
+				go(Vector2(-1, 0))
+			next_player()
+		6:
+			players[current_player].to_boat()
+			next_player()
 	$TextEdit.text = "Au joueur : %s" % current_player
 	pass
+
+func go(direction: Vector2):
+	# detect if theres forward colision
+	var current_pos: Vector2 = players[current_player].pos()
+	var next_pos: Vector2 = current_pos + direction
+	for i in range(player_count):
+		var pos: Vector2 = players[i].pos()
+		if i != current_player and not players[i].on_bark:
+			print("player different not on bark")
+			var vec_to_player = pos - current_pos
+			if round(vec_to_player.x*direction.y - vec_to_player.y*direction.x) == 0:
+				# vector are colinear
+				if (direction.x*vec_to_player.x >= 0 and direction.y*vec_to_player.y >= 0):
+					# the vectors are in the same direction
+					if sqrt(vec_to_player.x**2+vec_to_player.y**2) <= sqrt(direction.x**2+direction.y**2):
+						# The player is on the road
+						if direction.x==0:
+							players[i].go((next_pos-pos) + Vector2(0,1))
+						else:
+							players[i].go(direction)
+					
+	players[current_player].go(direction)
--- player.gd
+++ player.gd
@@ -1,21 +1,42 @@
 extends Node2D
 
-var wantedPos: Vector2 = Vector2.ZERO
+var init_pos: Vector2
+var wanted_pos: Vector2 = Vector2.ZERO
+var pos_before_bark: Vector2 = Vector2.ZERO
+var boat_counter: int = 0
 @export var on_bark: bool = false
 @export var on_bridge: bool = true 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite2D.frame = randi() % 5
-	wantedPos = $AnimatedSprite2D.position
+	init_pos = position
+	if position.x > get_window().size.x/2:
+		init_pos += Vector2(-50,0)
+	wanted_pos = position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$AnimatedSprite2D.position += (wantedPos - $AnimatedSprite2D.position) * delta * 2
+	position += (wanted_pos - position) * delta * 2
 	#print($AnimatedSprite2D.position)
-	if ($AnimatedSprite2D.position.y < -650):
+	if (position.y < -650):
 		on_bridge = false
-		on_bark = $AnimatedSprite2D.position.y > -720
+		on_bark = position.y > -720
 
-func forward(times: int=1):
-	wantedPos += times * Vector2(0, -55)
+func go(direction: Vector2):
+	wanted_pos += direction * 48
+	
+func to_boat():
+	pos_before_bark = position
+	wanted_pos = Vector2(296,424)
+	on_bark = true
+	boat_counter = 0
+
+func iterate_boat():
+	if boat_counter >= 1:
+		on_bark = false
+		wanted_pos = pos_before_bark
+	boat_counter += 1
+
+func pos():
+	return round((wanted_pos-init_pos) / 48)
